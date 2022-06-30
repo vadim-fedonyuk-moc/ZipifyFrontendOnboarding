0074156c08e00d875a8e87881bf26e18d66ddf20
vadim-fedonyuk-moc <vadim.fedonyuk@masterofcode.com>
2022-06-30 12:20:33 +0300

    add EditForm EditPage Component

---
+++ app-frontend/packs/components/editPage/EditForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "EditForm"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app-frontend/packs/components/editPage/EditorPage.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Editor Page</h1>
+</template>
+
+<script>
+export default {
+  name: "EditorPage"
+}
+</script>
+
+<style scoped>
+
+</style>
