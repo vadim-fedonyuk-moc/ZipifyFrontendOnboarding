bdb2ae0281c281073deae405ecfc18f90f864a7b
vadim-fedonyuk-moc <vadim.fedonyuk@masterofcode.com>
2022-06-30 12:18:46 +0300

    add ProjectBanner Component

---
+++ app-frontend/packs/components/sharedComponents/ProjectBanner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ba-project-banner">
+    <p class="ba-project-banner__text">Get your banner</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectBanner"
+}
+</script>
+
+<style scoped>
+.ba-project-banner {
+  margin: 0;
+  width: 100%;
+  height: 30px;
+  background: #7F56D9;
+}
+
+.ba-project-banner__text {
+  margin: 0;
+  color: #FFFFFF;
+  display: flex;
+  justify-content: center;
+  padding: 8px;
+}
+</style>
